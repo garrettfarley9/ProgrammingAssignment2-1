1dee1cfa0b6f350529475ea3139953ae71787e01
garrettfarley9 <garrett.farley9@gmail.com>
2020-04-17 18:56:42 -0500

    changed mean to matrix

--- courseraexample.R
+++ courseraexample.R
@@ -1,28 +1,29 @@
 #make a list of each element being a function
-makeVector <- function(x = numeric()) {
+makematx <- function(x = matrix()) {
         m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
         get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
+        setmatx <- function(matx) m <<- matx
+        getmatx <- function() m
         list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+             setmatx = setmatx,
+             getmatx = getmatx)
 }
 
 #if the mean has been calculated, it returns the mean. if not, it calculates
 #it and caches it
-cachemean <- function(x, ...) {
-        m <- x$getmean()
+cachematx <- function(x, ...) {
+        m <- x$getmatx()
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
         data <- x$get()
-        m <- mean(data, ...)
-        x$setmean(m)
+        m <- solve(data, ...)
+        x$setmatx(m)
         m
 }
+test<- matrix(data= c(4,2,7,6), nrow=2, ncol=2)
