3f2dfe3be32187f1f31793de51c254da9aa45e88
garrettfarley9 <garrett.farley9@gmail.com>
2020-04-18 11:19:24 -0500

    Both functions work properly.

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions work together to first create a list of functions
+##to store a cached matrix, then returns an inverse matrix either from the cache
+##or it solves for the inverse matrix and then stores it in the cache. 
 
-## Write a short comment describing this function
-#creates a special "matrix" object that can cache its inverse
+#Creates a list of functions "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-#solve(a) will return inverse of matrix(a)
-
-## Write a short comment describing this function
-#computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-#If the inverse has already been calculated (and the matrix has not changed), 
-#then the cachesolve should retrieve the inverse from the cache.
+                m <- NULL
+                set <- function(y) {
+                        x <<- y
+                        m <<- NULL
+                }
+                get <- function() x
+                setmatx <- function(matx) m <<- matx
+                getmatx <- function() m
+                list(set = set, get = get,
+                     setmatx = setmatx,
+                     getmatx = getmatx)
+        }
+        
+## Returns a matrix that is the inverse of 'x' either from cache or solve
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+                m <- x$getmatx()
+                if(!is.null(m)) {
+                        message("getting cached data")
+                        return(m)
+                }
+                data <- x$get()
+                m <- solve(data, ...)#solve(a) will return inverse of matrix(a)
+                x$setmatx(m)
+                m
+        }
+        
+my_matrix<-makeCacheMatrix(test)
+test<- matrix(data= c(4,2,7,6), nrow=2, ncol=2)
